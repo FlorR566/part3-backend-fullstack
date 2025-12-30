7aa5ab176659989cf4b4bdfecb5c7b03d46a86db
FlorR566 <florodriguez566@gmail.com>
2025-12-30 17:54:53 -0300

    Add /info endpoint to expose phonebook statistics

--- index.js
+++ index.js
@@ -30,6 +30,10 @@ app.get("/", (request, response) => {
 	response.send("<h1>Hello World!</h1>");
 });
 
+app.get("/info", (request, response) => {
+	response.send(`<p>Phonebook has info for ${persons.length} books</p>`);
+});
+
 app.get("/api/persons", (request, response) => {
 	response.json(persons);
 });
