c3eff1763d0f1589007b2abfd0fedf1e06a98e29
FlorR566 <florodriguez566@gmail.com>
2025-12-30 10:42:38 -0300

    Add phonebook backend with GET /api/persons

--- index.js
+++ index.js
@@ -3,21 +3,26 @@ const app = express();
 
 app.use(express.json());
 
-let notes = [
+let persons = [
 	{
 		id: 1,
-		content: "HTML is easy",
-		important: true,
+		name: "Arto Hellas",
+		number: "040-123456",
 	},
 	{
 		id: 2,
-		content: "Browser can execute only JavaScript",
-		important: false,
+		name: "Ada Lovelace",
+		number: "39-44-5323523",
 	},
 	{
 		id: 3,
-		content: "GET and POST are the most important methods of HTTP protocol",
-		important: true,
+		name: "Dan Abramov",
+		number: "12-43-234345",
+	},
+	{
+		id: 4,
+		name: "Mary Poppendieck",
+		number: "39-23-6423122",
 	},
 ];
 
@@ -25,27 +30,27 @@ app.get("/", (request, response) => {
 	response.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/notes", (request, response) => {
-	response.json(notes);
+app.get("/api/persons", (request, response) => {
+	response.json(persons);
 });
 
-app.get("/api/notes/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response) => {
 	const id = Number(request.params.id);
-	const note = notes.find((note) => note.id === id);
+	const person = persons.find((person) => person.id === id);
 
-	if (note) {
-		response.json(note);
+	if (person) {
+		response.json(person);
 	} else {
 		response.status(404).end();
 	}
 });
 
 const generateId = () => {
-	const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+	const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
 	return maxId + 1;
 };
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/persons", (request, response) => {
 	const body = request.body;
 
 	if (!body.content) {
@@ -54,20 +59,20 @@ app.post("/api/notes", (request, response) => {
 		});
 	}
 
-	const note = {
+	const person = {
 		content: body.content,
 		important: Boolean(body.important) || false,
 		id: generateId(),
 	};
 
-	notes = notes.concat(note);
+	persons = persons.concat(person);
 
-	response.json(note);
+	response.json(person);
 });
 
-app.delete("/api/notes/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response) => {
 	const id = Number(request.params.id);
-	notes = notes.filter((note) => note.id !== id);
+	persons = persons.filter((person) => person.id !== id);
 
 	response.status(204).end();
 });
