94c1b28216346265dd2e09f29afc3c463e7b7f24
FlorR566 <florodriguez566@gmail.com>
2026-01-12 16:11:42 -0300

    Add comments and fetch notes from database

--- mongo.js
+++ mongo.js
@@ -1,31 +1,57 @@
+// Importar Mongoose
 const mongoose = require("mongoose");
 
+// Forma simple y segura de no hardcodear credenciales
 if (process.argv.length < 3) {
 	console.log("give password as argument");
 	process.exit(1);
 }
 
+// Obtener la password
 const password = process.argv[2];
 
+// URI de conexión a MongoDB Atlas
 const url = `mongodb+srv://fullstack:${password}@cluster0.sminnmi.mongodb.net/noteApp?retryWrites=true&w=majority`;
 
+// Configuración de Mongoose
 mongoose.set("strictQuery", false);
 
+// Conexión a la base
 mongoose.connect(url);
 
+// Definición del schema
 const noteSchema = new mongoose.Schema({
 	content: String,
 	important: Boolean,
 });
 
+// Crear el modelo (clase que representa una colección)
 const Note = mongoose.model("Note", noteSchema);
 
-const note = new Note({
-	content: "HTML is easy",
-	important: true,
-});
+// //Crear objetos
+// const note = new Note({
+// 	content: "Mongoose makes things easy",
+// 	important: true,
+// });
 
-note.save().then((result) => {
-	console.log("note saved!");
+// // Guardar objetos en la base
+// note.save().then((result) => {
+// 	console.log("note saved!");
+// 	mongoose.connection.close();
+// });
+
+// Obtener objetos de la DB
+Note.find({}).then((result) => {
+	result.forEach((note) => {
+		console.log(note);
+	});
 	mongoose.connection.close();
 });
+
+// // Restringir la búsqueda para encontrar sólo notas importantes
+// Note.find({ important: true }).then((result) => {
+// 	result.forEach((note) => {
+// 		console.log(note);
+// 	});
+// 	mongoose.connection.close();
+// });
